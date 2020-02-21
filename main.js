067659b4bca10cac12d82dc4034d87520c7d3b1a
Jackie Chen <jchen@deputy.com>
2020-02-21 11:35:59 +1100

    add builder

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow() {
 	mainWindow.loadURL('https://once.deputy.com')
 
 	// Open the DevTools.
-	mainWindow.webContents.openDevTools()
+	// mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
